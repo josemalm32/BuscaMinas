64a542c5d0d7825ca6c3bb9a0fc0e653aee74219
josemalm32 <josema.aa14@gmail.com>
2017-04-02 13:26:02 +0200

    Commit Inicial. Creacion de Botones realizada

--- control/ReceptorEventos.java
+++ control/ReceptorEventos.java
@@ -13,19 +13,13 @@ public class ReceptorEventos implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		//marcador.marcarCasilla((JButton) e.getSource(), tablero);
-		Object eventSource = arg0.getSource();
-       JButton clickedButton = (JButton) eventSource;
-        String name = clickedButton.getName();
-
-           String[] xy = clickedButton.getName().split(" ", 2);
-           int x = Integer.parseInt(xy[0]);
-           int y = Integer.parseInt(xy[1]);
+		   JButton botonpulsado = (JButton) arg0.getSource();
+           String[] posicionBoton = botonpulsado.getName().split(" ", 2);
+           int x = Integer.parseInt(posicionBoton[0]);
+           int y = Integer.parseInt(posicionBoton[1]);
+           //Para Debug
            System.out.println(x);
            System.out.println(y);
-
-//       
-
 	}
 
 }
